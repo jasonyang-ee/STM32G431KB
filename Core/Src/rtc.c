d7830ae01f905337d4203d93bf75b1f71478efbc
Jason Teng <jasonyang.ee@pm.me>
2023-07-12 00:13:15 -0700

    Fix: LSI 32khz and use 249 for synchprediv

--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -46,7 +46,7 @@ void MX_RTC_Init(void)
   hrtc.Instance = RTC;
   hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
   hrtc.Init.AsynchPrediv = 127;
-  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.SynchPrediv = 249;
   hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
   hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
   hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
